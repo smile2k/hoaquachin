b7d3aafe1b722923a85d406acf9169464e4c4f0f
smile2k <72849346+smile2k@users.noreply.github.com>
2021-12-28 20:00:25 +0700

    Update quachin.py

--- quachin.py
+++ quachin.py
@@ -10,7 +10,7 @@ red_mask = cv2.inRange(hsv_frame,low_red,high_red)
 contours1, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 if len(contours1) != 0:
     for contour in contours1:
-        if cv2.contourArea(contour) > 1200:
+        if cv2.contourArea(contour) > 500:
             x,y,w,h = cv2.boundingRect(contour)
             cv2.rectangle(img, (x,y),(x+w,y+h),(0,0,255),3)
             cv2.putText(img, "ripen ", (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 255), 2)
